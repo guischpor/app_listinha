81dbde46f82668ee2b8fa69119d2b645f20fe25b
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2023-02-25 13:05:35 -0300

    config projeto

--- backend_listinha/bin/backend_listinha.dart
+++ backend_listinha/bin/backend_listinha.dart
@@ -1,5 +1,7 @@
+
 import 'package:backend_listinha/backend_listinha.dart' as backend_listinha;
 
 void main(List<String> arguments) {
   print('Hello world: ${backend_listinha.calculate()}!');
+  // stdin.readLineSync();
 }
